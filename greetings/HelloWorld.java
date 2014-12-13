300cc966e215e2c8d4bbff9a7a30f94cc8847efa
ix <tyrellthor@live.com>
2014-12-13 02:44:29 -0500

    first files

---
+++ greetings/HelloWorld.java
@@ -0,0 +1,13 @@
+package greetings;
+
+public class HelloWorld {
+    private static int age = 50;
+    public static void main(String args[]) {
+        int number = 3;
+        number = number + age;
+        number++;
+        System.out.println(number);
+    }
+
+
+}
